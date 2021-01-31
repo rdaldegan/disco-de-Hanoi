34b9aac6bd7d0fcfede91c399051d6f99240bd12
rdaldegan <ra.fa.daldegan@hotmail.com>
2021-01-31 19:50:45 -0300

    comit2

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ const Main = styled.div`
   height: 890px;
   border: solid 1px;
   border-radius: 50%;
-
 `
 const Image = styled.img`
   position: absolute;
@@ -16,14 +15,20 @@ const Image = styled.img`
   left: ${props => props.left}px;
   transform: rotate(${props => props.rot}deg);
   transition: transform 0.5s ease-in;
+  border: solid ${props => props.selec}px yellow;
 `;
 
 
 export default function Home() {
   
+  const [selec0, setSelec0] = useState([2])
+  const [selec1, setSelec1] = useState([0])
+  const [selec2, setSelec2] = useState([0])
+  const [selec3, setSelec3] = useState([0])
+
   const [vitoria, setVitoria] = useState(false)
   const [selecDisk, setSelecDisk] = useState([0])
-  const [discos, setDiscos] = useState([120, 0, 0, 0])
+  const [discos, setDiscos] = useState([0, 120, 240, 0])
 
   useEffect( () => {
     window.addEventListener("keydown", comandHandler)
@@ -41,6 +46,37 @@ export default function Home() {
     }
     else {
       return 3
+    }
+  }
+
+  function selecionaBordas(disco){
+    if(disco == 0){
+      setSelec0([2])
+      setSelec1([0])
+      setSelec2([0])
+      setSelec3([0])
+      console.log(selec0,selec1,selec2,selec3)
+    }
+    else if(disco == 1){
+      setSelec0([2])
+      setSelec1([2])
+      setSelec2([0])
+      setSelec3([0])
+      console.log(selec0,selec1,selec2,selec3)
+    }
+    else if(disco == 2){
+      setSelec0([0])
+      setSelec1([2])
+      setSelec2([2])
+      setSelec3([0])
+      console.log(selec0,selec1,selec2,selec3)
+    }
+    else if(disco == 3){
+      setSelec0([0])
+      setSelec1([0])
+      setSelec2([2])
+      setSelec3([2])
+      console.log(selec0,selec1,selec2,selec3)
     }
   }
 
@@ -73,6 +109,8 @@ export default function Home() {
       let state = selecDisk
       state[0] = novoDisco
       setSelecDisk(state)
+      selecionaBordas(selecDisk[0])
+      console.log(selecDisk)
     }
     else if(key.keyCode == 40){
       novoDisco = selecDisk[0] - 1
@@ -82,6 +120,8 @@ export default function Home() {
       let state = selecDisk
       state[0] = novoDisco
       setSelecDisk(state)
+      selecionaBordas(selecDisk[0])
+      console.log(selecDisk)
     }
     let direcao
     if(key.keyCode == 37){
@@ -92,7 +132,6 @@ export default function Home() {
       giraDisco(selecDisk[0], direcao)
     }
     if(discos[0] == discos[1] && discos[0] == discos[2] && discos[0] == discos[3]){
-
       setTimeout(()=>{
         let vit = vitoria
       vit = true
@@ -107,10 +146,10 @@ export default function Home() {
     <Main>
       {vitoria === false ?
       <div>
-        <Image src='/disco3.png' width={890} height={890} top={0} left={0} rot={discos[3]}/>
-        <Image src='/disco2.png' width={688} height={688} top={101} left={101} rot={discos[2]}/>
-        <Image src='/disco1.png' width={482} height={482} top={204} left={204} rot={discos[1]}/>
-        <Image src='/disco0.png' width={316} height={316} top={287} left={287} rot={discos[0]}/>
+        <Image src='/disco3.png' width={890} height={890} top={0} left={0} selec={selec3} rot={discos[3]}/>
+        <Image src='/disco2.png' width={688} height={688} top={101} left={101} selec={selec2} rot={discos[2]}/>
+        <Image src='/disco1.png' width={482} height={482} top={204} left={204} selec={selec1} rot={discos[1]}/>
+        <Image src='/disco0.png' width={316} height={316} top={287} left={287} selec={selec0} rot={discos[0]}/>
       </div>
       :
       <div>
